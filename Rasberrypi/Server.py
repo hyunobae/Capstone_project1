6671d8fe779765eab75c33a26713561935706d97
sangmeeee <goodman1223@naver.com>
2020-11-13 05:27:51 +0900

    Rasberrypi

--- Rasberrypi/Server.py
+++ Rasberrypi/Server.py
@@ -2,6 +2,8 @@ import socket
 import argparse
 import threading
 import time
+import os
+import shutil
 
 host = ''
 port = 10000
@@ -22,33 +24,49 @@ def handle_receive(client_socket, addr):
     print("connect with " + str(addr))
     count = 0
     string = str(addr)
+    #ip와 socketnum 얻어오기
+
+    dir_name = os.getcwd()
+    dir_name += "/Client" +str(user_list.index(addr))
+    #받아온 client별로 폴더 이름 지정
+
+    try:
+        shutil.rmtree(dir_name) #디렉토리가 존재하면 삭제
+    except Exception as e:
+        print(dir_name)
     
+    os.makedirs(dir_name)#디렉토리 생성
     
     while 1:
         file_length = client_socket.recv(1024)
+        #file 크기를 받는 과정
         client_socket.send("str".encode())
         file_length = file_length.decode()
+        #file_length에는 server가 받아올 file 크기가 저장됨
         #print(file_length)
         if not file_length:
             break
 
         length = 0
         data = []
+        #file을 붙이는 과정
         while 1:
             d = client_socket.recv(4096)
             data.append(d)
+            #data에 4096바이트씩 받아오면서 연결해줌
             length += len(d)
             #print(length)
             if int(length) == int(file_length):
                 #print('break')
                 break
 
-        stri = string + str(count) + ".jpg"
+        stri = dir_name + "/" + string + str(count) + ".jpg" #jpg 파일 생성
         count+=1
-        print(str(addr) + " : " + stri)
+        #print(str(addr) + " : " + stri)
         with open(stri, 'wb') as f:
             for a in data:
                 f.write(a)
+
     print("disconnect with " + str(addr))
     user_list.remove(addr)
 
@@ -63,7 +81,8 @@ def accept_func():
     server_socket.bind((host, port))
 
     server_socket.listen(5)
-
+    #최대 5개 가능
+    
     while 1:
         client_socket, addr = server_socket.accept()
 
